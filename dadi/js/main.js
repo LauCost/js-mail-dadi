290958c9f252d614b0819360e15cfed8dfef46e6
Laurentiu <lcostin40@gmail.com>
2021-10-14 17:11:32 +0200

    Corretto codice riguardante la creazione del numero casuale ed aggiunto codicce della verifica del vincitore.

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,11 +1,25 @@
 //Seleziona elemento html sul quale poi stampera il numero
-const numeroUtente = document.querySelector(".utente");
+const utente = document.querySelector(".utente")
+
+//Genera il numero per l'utente
+const numeroUtente = Math.floor(Math.random() * 6) + 1;
+
+console.log(numeroUtente);
 
 //Stampa il numero casuale per l'utente
-numeroUtente.innerHTML = "Numero casuale dell'Utente: " + Math.floor((Math.random() * 6) + 1);
+utente.innerHTML = "Numero casuale dell'Utente: " + numeroUtente;
 
 //Seleziona elemento html sul quale poi stampera il numero
-const numeroPc = document.querySelector(".pc");
+const pc = document.querySelector(".pc");
+
+const numeroPc = Math.floor(Math.random() * 6) + 1;
 
 //Stampa il numero casuale per il PC
-numeroPc.innerHTML = "Numero casuale del PC: " + Math.floor((Math.random() * 6) + 1);
+pc.innerHTML = "Numero casuale del PC: " + numeroPc;
+
+const risultato = document.querySelector(".risultato")
+if (numeroUtente <= numeroPc) {
+    risultato.innerHTML = "Hai Perso!"
+} else {
+    risultato.innerHTML = "Hai Vinto!"
+}
