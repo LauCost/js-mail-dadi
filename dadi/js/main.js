08a393d5a5fdc7df995ee042623b0a03fda39f1e
Laurentiu <lcostin40@gmail.com>
2021-10-14 16:19:22 +0200

    Inserito codice per richedere l'isnerimento di un numero

---
+++ dadi/js/main.js
@@ -0,0 +1,12 @@
+//Chiede all'utente di inserire un numero da 1 a 6
+const numeroUtente = prompt("Inserisci un numero da 1 a 6");
+
+/* Number(numeroUtente); */
+
+console.log(Number(numeroUtente));
+
+const numeri = [1, 2, 3, 4, 5, 6];
+
+for (i = 0; i < numeri.length; i++) {
+    if 
+}
