50a4c68ac7ea9fddda533951865f3df80c6283d3
Laurentiu <lcostin40@gmail.com>
2021-10-14 17:15:58 +0200

    Aggiunto il pareggio e vari commenti.

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -4,22 +4,36 @@ const utente = document.querySelector(".utente")
 //Genera il numero per l'utente
 const numeroUtente = Math.floor(Math.random() * 6) + 1;
 
-console.log(numeroUtente);
-
 //Stampa il numero casuale per l'utente
 utente.innerHTML = "Numero casuale dell'Utente: " + numeroUtente;
 
 //Seleziona elemento html sul quale poi stampera il numero
 const pc = document.querySelector(".pc");
 
+//Genera il numero per il PC
 const numeroPc = Math.floor(Math.random() * 6) + 1;
 
 //Stampa il numero casuale per il PC
 pc.innerHTML = "Numero casuale del PC: " + numeroPc;
 
+//Seleziona elemento html sul quale stampa l'esito
 const risultato = document.querySelector(".risultato")
-if (numeroUtente <= numeroPc) {
-    risultato.innerHTML = "Hai Perso!"
-} else {
+
+//Verifica se è un pareggio
+if (numeroUtente == numeroPc) {
+
+    //Stamapa il risultato
+    risultato.innerHTML = "Pareggio!"
+
+    //Verifica se l'utente ha vinto
+} else if (numeroUtente > numeroPc) {
+
+    //Stamapa il risultato
     risultato.innerHTML = "Hai Vinto!"
+
+    //Verifica se l'utente ha perso
+} else {
+
+    //Stamapa il risultato
+    risultato.innerHTML = "Hai Perso"
 }
