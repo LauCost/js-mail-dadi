743c1567ed5defded2505690265a1972eff3b160
Laurentiu <lcostin40@gmail.com>
2021-10-14 16:30:50 +0200

    Tentativo di verificare se il valore inserito dall'utente corrisponde alla richiesta

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,12 +1,17 @@
 //Chiede all'utente di inserire un numero da 1 a 6
-const numeroUtente = prompt("Inserisci un numero da 1 a 6");
+const numeroUtente = Number(prompt("Inserisci un numero da 1 a 6"));
 
 /* Number(numeroUtente); */
 
-console.log(Number(numeroUtente));
+console.log(numeroUtente);
 
 const numeri = [1, 2, 3, 4, 5, 6];
 
+console.log(numeri);
+
+
 for (i = 0; i < numeri.length; i++) {
-    if 
+    if (numeroUtente != numeri[0]) {
+
+    }
 }
