161d97f7b7ee8095c308aadc9a0efdf7d30820a5
Laurentiu <lcostin40@gmail.com>
2021-10-14 15:43:20 +0200

    inserite le email autorizzate

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 //Chide all'utente la sua email
-const email = prompt("Inserisci la tua email");
-
+const email = prompt("Inserisci la tua email").toLowerCase();
 
 console.log(email);
+
+//Sono le email autorizzate
+const autorizzate = ["lau@gmail.com, leo@gmail.com, fabio@gmail.com, tiziano@gmail.com, fabiola@gmail.com"]
+
+console.log(autorizzate);
+
+
+
+
 
 
 
